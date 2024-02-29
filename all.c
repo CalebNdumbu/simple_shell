5eaa3201f2ff74b94ace3d70df900b9d20a0e7a0
Caleb <Calebkyamaweb@gmail.com>
2024-02-29 21:29:45 +0300

    last commit of the day

--- all.c
+++ all.c
@@ -5,10 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <sys/wait.h>
 
 #define DELIM " \n\t\r\a"
@@ -23,9 +21,165 @@ extern char **environ;
 
 #endif
 
-/**
- * print_environment - print the current environment variables
- */
+char *get_location(char *cmd)
+{
+    char *path;
+    char *path_cpy;
+    char *path_token;
+    char *file_path;
+    int cmd_length;
+    int dir_len;
+    struct stat buffer;
+
+    path = getenv("PATH");
+
+    if (path)
+    {
+        path_cpy = strdup(path);
+        cmd_length = strlen(cmd);
+        path_token = strtok(path_cpy, ":");
+
+        while (path_token != NULL)
+        {
+            dir_len = strlen(path_token);
+            file_path = malloc(cmd_length + dir_len + 2);
+
+            strcpy(file_path, path_token);
+            strcat(file_path, "/");
+            strcat(file_path, cmd);
+
+            if (stat(file_path, &buffer) == 0)
+            {
+                free(path_cpy);
+                return file_path;
+            }
+            else
+            {
+                free(file_path);
+                path_token = strtok(NULL, ":");
+            }
+        }
+
+        free(path_cpy);
+
+        if (stat(cmd, &buffer) == 0)
+        {
+            return strdup(cmd);
+        }
+
+        return NULL;
+    }
+
+    return NULL;
+}
+
+void execmd(char **argv)
+{
+    pid_t pid;
+    int status;
+
+    pid = fork();
+
+    if (pid == 0)
+    {
+        char *cmd;
+        char *actual_cmd;
+
+        if (argv)
+        {
+            cmd = argv[0];
+            actual_cmd = get_location(cmd);
+
+            if (actual_cmd != NULL)
+            {
+                char *const *const_argv = (char *const *)argv;
+
+                if (execve(actual_cmd, const_argv, NULL) == -1)
+                {
+                    perror("execve");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else
+            {
+                printf("Command not found: %s\n", cmd);
+                exit(EXIT_FAILURE);
+            }
+        }
+        exit(EXIT_SUCCESS);
+    }
+    else if (pid < 0)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        do
+        {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+}
+
+char **split_line(char *line)
+{
+    int position = 0;
+    int buffersize = 64;
+    char *token;
+    char **tokens;
+    char **new_tokens;
+
+    tokens = malloc(sizeof(char *) * buffersize);
+
+    if (!tokens)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, DELIM);
+
+    while (token != NULL)
+    {
+        tokens[position] = token;
+        position++;
+
+        if (position >= buffersize)
+        {
+            buffersize += 64;
+            new_tokens = realloc(tokens, sizeof(char *) * buffersize);
+
+            if (!new_tokens)
+            {
+                perror("realloc");
+                free(tokens);
+                exit(EXIT_FAILURE);
+            }
+            tokens = new_tokens;
+        }
+        token = strtok(NULL, DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+char *line_reader(void)
+{
+    char *line = NULL;
+    size_t bufsize = 0;
+    ssize_t read;
+
+    read = getline(&line, &bufsize, stdin);
+    if (read == -1)
+    {
+        perror("getline");
+        exit(EXIT_FAILURE);
+    }
+
+    return line;
+}
+
 void print_environment(void)
 {
     char **env_var = environ;
@@ -37,172 +191,36 @@ void print_environment(void)
     }
 }
 
-/**
- * get_location - get the location of a command
- * @cmd: the command
- *
- * Return: the location of the command, or NULL if not found
- */
-char *get_location(char *cmd)
+int main(void)
 {
-    char *path;
-    @ @-33, 10 + 55, 8 @ @ char *get_location(char *cmd)
+    char *line;
+    char **args;
 
-                         if (path)
+    do
     {
+        printf("$ ");
+        line = line_reader();
+        args = split_line(line);
 
-        path_cpy = strdup(path);
-        cmd_length = strlen(cmd);
-
-        path_token = strtok(path_cpy, ":");
-
-        while (path_token != NULL)
-            @ @-74, 23 + 94, 30 @ @ char *get_location(char *cmd) return (NULL);
-    }
-
-#include <sys/types.h>
-#include <sys/wait.h>
-
-    /**
-     * execmd - execute a command
-     * @argv: array of command arguments
-     */
-    void execmd(char **argv)
-    {
-        pid_t pid;
-        int status;
-
-        pid = fork();
-
-        if (pid == 0)
-            if (argv)
-            {
-                char *cmd;
-                char *actual_cmd;
-                if (strcmp(argv[0], "env") == 0)
-                {
-                    print_environment();
-                    return;
-                }
-
-                if (argv)
-                    pid = fork();
-
-                if (pid == 0)
-                {
-                    char *cmd;
-                    char *actual_cmd;
-
-                    cmd = argv[0];
-                    actual_cmd = get_location(cmd);
-
-                    @ @-109, 30 + 136, 28 @ @ void execmd(char **argv) printf("Command not found: %s\n", cmd);
-                    exit(EXIT_FAILURE);
-                }
-                exit(EXIT_SUCCESS);
-            }
-        exit(EXIT_SUCCESS);
-    }
-    else if (pid < 0)
-    {
-        perror("Error in forking");
-    }
-    else
-    {
-        do
-            else if (pid < 0)
-            {
-                waitpid(pid, &status, WUNTRACED);
-            }
-        while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        perror("Error in forking");
-    }
-    else
-    {
-        do
+        if (args != NULL && args[0] != NULL)
         {
-            waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-}
-}
-
-/**
- * split_input - tokenizes a string
- *
- * @line: string to be tokenized.
- * split_line - tokenizes a string
- * @line: string to be tokenized
- *
- * Return: ptr to tokenized array.
- * Return: pointer to tokenized array
- */
-
-char **split_line(char *line)
-{
-    int position;
-    @ @-176, 9 + 201, 13 @ @ char **split_line(char *line) return tokens;
-}
-
-/**
- * line_reader - reads a line of input from stdin
- *
- * Return: pointer to the line read
- */
-char *line_reader(void)
-{
-
-    char *linepointer = NULL;
-    size_t buf_size = 0;
-
-    @ @-187, 7 + 216, 6 @ @ char *line_reader(void) if (feof(stdin))
-    {
-    }
-
-    else
-    {
-        perror("Error reading from stdin");
-        @ @-197, 21 + 225, 39 @ @ char *line_reader(void) return (linepointer);
-    }
-
-    int main(int ac, char **argv)
-        /**
-         * main - entry point of the program
-         * @ac: argument count
-         * @argv: array of argument strings
-         *
-         * Return: always 0
-         */
-        int main(void)
-    {
-        char *line;
-        char **args;
-        (void)ac;
-        (void)argv;
-
-        do
-        {
-            printf("shell by caleb $ ");
-            printf("$ ");
-            line = line_reader();
-            args = split_line(line);
-            execmd(args);
-
-            if (args != NULL && args[0] != NULL)
+            if (strcmp(args[0], "env") == 0)
             {
-                if (strcmp(args[0], "env") == 0)
-                {
-                    print_environment();
-                }
-                else
-                {
-                    execmd(args);
-                }
+                print_environment();
             }
+            else if (strcmp(args[0], "exit") == 0)
+            {
+                break;
+            }
+            else
+            {
+                execmd(args);
+            }
+        }
 
-            free(line);
-            free(args);
-        } while (1);
+        free(line);
+        free(args);
+    } while (1);
 
-        return (0);
-    }
+    return 0;
+}
